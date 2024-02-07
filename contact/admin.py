e37e417888c3b36366fd9a394d851cddcbc467f0
Evandro Calado <evandrocalado07@gmail.com>
2024-02-07 20:41:17 -0300

    ✨ feat(admin): list config

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name',
+                    'email', 'phone', 'created_at',)
+    ordering = ('-id',)
+    list_filter = ('created_at',)
+    search_fields = ('id', 'first_name', 'last_name', 'email',)
+    list_per_page = 10
+    list_max_show_all = 100
