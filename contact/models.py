7d00df35e218c8146a45d79407d3cf8d2b517359
Evandro Calado <evandrocalado07@gmail.com>
2024-02-07 18:21:33 -0300

    🐳 chore(models): registration model
    registration model contact on admin

--- contact/models.py
+++ contact/models.py
@@ -9,3 +9,6 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
