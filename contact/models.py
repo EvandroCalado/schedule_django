8e892f24d35e38813c9e3175da1477ca6e727c6a
Evandro Calado <evandrocalado07@gmail.com>
2024-02-07 23:19:21 -0300

    🐳 chore(models): category model

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50,)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -12,6 +23,8 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(
         blank=True, upload_to='media/pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True, )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
