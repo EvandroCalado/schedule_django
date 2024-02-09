553acc117e57e98d00ceeaf1899cd79e7e0b61d6
Evandro Calado <evandrocalado07@gmail.com>
2024-02-09 09:58:00 -0300

    🐳 chore(views): add filter by show

--- contact/admin.py
+++ contact/admin.py
@@ -11,9 +11,10 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name',
-                    'email', 'phone', 'created_at',)
+                    'email', 'phone', 'created_at', 'show',)
     ordering = ('-id',)
     list_filter = ('created_at',)
     search_fields = ('id', 'first_name', 'last_name', 'email',)
     list_per_page = 10
     list_max_show_all = 100
+    list_editable = ('show',)
