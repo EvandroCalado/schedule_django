d33aa29da078327a91ea930c9befa0a5e7437800
Evandro Calado <evandrocalado07@gmail.com>
2024-02-10 17:58:38 -0300

    ✨ feat(pagination): create pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
 
-    print(contacts.query)
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': ' - Contacts',
     }
 
@@ -46,10 +49,12 @@ def search(request):
         )\
         .order_by('-id')
 
-    print(contacts.query)
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': f' - Search: {search_value}',
         'search_value': search_value,
     }
